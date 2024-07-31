5f637bba5754e47080aaff51152aafed955b344c
hydedada0129 <48003734+hydedada0129@users.noreply.github.com>
2024-07-31 09:28:51 +0800

    Update drawing.py

--- drawing.py
+++ drawing.py
@@ -24,7 +24,7 @@ def authenticate_gmail_api():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("client_secret_drawing.json", SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
             creds = flow.run_local_server(port=8080)
         with open("token.json", "w") as token:
             token.write(creds.to_json())
